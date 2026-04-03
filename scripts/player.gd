b4436e6fb3172761a0b921606575ac0e17dc4b94
Le Hoang Thanh Dat <datlhthanh@gmail.com>
2026-04-03 17:24:57 +0700

    feat: add UI labels, score tracking

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,9 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
-
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
